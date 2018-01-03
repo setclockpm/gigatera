81940028fac195dc9613e4ca3e3c213590fa3bd8
Phillip Medrano <phillip.medrano@gmail.com>
2018-01-02 17:22:14 -0800

    added custom product templates

---
+++ astrid-child/page-templates/page_products_home.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+
+Template Name: Products Landing
+
+*/
+	get_header();
+?>
+
+
+
+	<div id="primary" class="content-area fullwidth">
+	  <main id="main" class="site-main" role="main">
+          
+             
+	  	<?php while ( have_posts() ) : the_post(); ?>
+          
+	  		<?php get_template_part( 'template-parts/content', 'products' ); ?>
+	  		
+	  	<?php endwhile; // end of the loop. ?>
+          
+          
+	  </main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer("products"); ?>
+
