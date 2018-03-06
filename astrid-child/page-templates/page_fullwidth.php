0e2b2f5ca66366bb4e31e123a213012655df265c
Phillip Medrano <phillip.medrano@gmail.com>
2018-03-05 19:34:03 -0800

    template for fullwidth created

--- astrid-child/page-templates/page_fullwidth.php
+++ astrid-child/page-templates/page_fullwidth.php
@@ -7,40 +7,12 @@ Template Name: Full width
 */
 	get_header();
 ?>
-
-<!-- </div>#container -->
-
-
-	<div id="primary" class="content-area fullwidth">
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				<?php
-
-					if ( comments_open() || '0' != get_comments_number() ) :
-
-						comments_template();
-
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-
-
-	</div><!-- #container -->
+</div><!-- #container -->
 
 <div id="primary" class="content-area fullwidth">
 
 <?php if (has_post_thumbnail()): ?>
-  <div class="subsection-title-container" style="background-image:url(<?php the_post_thumbnail_url( "full" ) ?>);">
+  <div class="subsection-title-container cover-no-repeat" style="background-image:url(<?php the_post_thumbnail_url( "full" ) ?>);">
 <?php else: ?>
   <div class="subsection-title-container">
 <?php endif ?>
@@ -64,7 +36,7 @@ Template Name: Full width
       <div class="lighting-content row">
 
         <?php while ( have_posts() ) : the_post(); ?>
-          <?php get_template_part( 'template-parts/content', 'lighting' ); ?>
+          <?php get_template_part( 'template-parts/content', 'plain' ); ?>
         <?php endwhile; // end of the loop. ?>
 
       </div> 
---
+++ astrid-child/template-parts/content-plain.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template part for displaying page content in page_fullwidth.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astrid
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class("col-md-12 under-fixed-nav"); ?>>
+
+  <div class="entry-content">
+  	<?php if (get_post_custom_values('subheader-img-url')) : ?>
+      <div class="subheader-img">
+        <img src="<?php echo get_post_meta($post->ID, 'subheader-img-url', true); ?>">
+      </div>
+    <?php endif; ?> 
+
+    <?php the_content(); ?>
+
+    <?php if (get_post_custom_values('prefooter-img-url')) : ?>
+      <div class="prefooter-img">
+        <img src="<?php echo get_post_meta($post->ID, 'prefooter-img-url', true); ?>">
+      </div>
+    <?php endif; ?> 
+    
+
+  </div><!-- .entry-content -->
+
+</article><!-- #post-## -->
