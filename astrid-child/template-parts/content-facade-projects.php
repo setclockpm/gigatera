61413e135d18018a2167a87d6ed334d03bcc899e
Phillip Medrano <phillip.medrano@gmail.com>
2018-03-21 20:06:12 -0700

    facade case study template

--- astrid-child/template-parts/content-facade-projects.php
+++ astrid-child/template-parts/content-facade-projects.php
@@ -1,50 +1,65 @@
-<div id="facade-series" class="under-fixed-nav">
+<?php
+/**
+ * Template part for displaying page content in page_facade_projects.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astrid
+ */
 
-  <div class="left col-md-4 col-sm-6 col-xs-12">
-    <div id="incheon-bridge" class="col-xs-12 card">
-      <div class="card-block">
-        <div class="thumbnail top">
-          <iframe width="280" src="https://www.youtube.com/embed/alSb2pdkXbI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        </div>
-        <div class="horizontal-divider">
-          <img src="https://gigaterausa.com/wp-content/uploads/2018/01/divider.png">
-        </div>
-        <div class="card-text bottom">
-          <h3 class="title">Incheon Bridge SFX-RGB</h3>
-          <p class="description">
-            SUFA-X RGB 400W, Incheon Bridge Facade Lighting Installation, Korea
-          </p>
-        </div>
-      </div>
-      <img class="card-img-bottom" src="https://gigaterausa.com/wp-content/uploads/2018/01/giga-divider.jpg" alt="Card Bottom Border">
-    </div>
+?>
+
+
+<header class="entry-header">
+  <h3 id="main-title" itemprop="headline" class="tagline applications-tagline">
+    <?php echo(get_post_meta($post->ID, 'headline', true)); ?>
+  </h3>
+</header><!-- .entry-header -->
+ 
+
+<div class="entry-content">
+  <div class="intro">
+    <p><?php echo(get_post_meta($post->ID, 'headline-blurb', true)); ?></p>
   </div>
+  
+  <div id="sports-flood-series" class="under-fixed-nav">
+    <ul class="horiz-card-list">
 
-
-
-
-  <div class="middle col-md-4 col-xs-12">
-
-    <div id="incheon-highmast" class="col-xs-12 card">
-      <div class="card-block">
-        <div class="thumbnail top">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/JcNBSjHZ0B0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      <li id="incheon-bridge" class="card">
+        <div class="row">
+          <div class="card-media col-xs-5 col-sm-3 col-md-3">
+            <iframe width="190" src="https://www.youtube.com/embed/alSb2pdkXbI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+          <div class="small-label col-xs-6 col-sm-3 col-md-3 text-center non-mobile">
+            <span class="case-study exists">
+              <a href="/incheon-bridge-facade/">
+                <i class="fa fa-area-chart fa-2x" aria-hidden="true"></i>
+                <i class="fa fa-search-plus" aria-hidden="true"></i><br>
+                <span style="font-size:0.7em;"">Case Study</span>
+              </a>
+            </span>
+          </div>
+          <div class="card-stacked col-xs-12 col-sm-6 col-md-6">
+            <span class="installation-site">Incheon Bridge Facade</span>
+            <span class="location">
+              <a href="https://goo.gl/maps/aKzAeU6py7s">Incheon, South Korea</a>
+            </span>
+          </div>
         </div>
-        <div class="horizontal-divider">
-          <img src="https://gigaterausa.com/wp-content/uploads/2018/01/divider.png">
-        </div>
-        <div class="card-text bottom">
-          <h3 class="title">Incheon Bridge Highmast</h3>
-          <p class="description">
-            MAHA 400W, Incheon Bridge Hightmast Pilot Installation Reference Video, Korea
-          </p>
-        </div>
-      </div>
-      <img class="card-img-bottom" src="https://gigaterausa.com/wp-content/uploads/2018/01/giga-divider.jpg" alt="Card Bottom Border">
-    </div>
-    
-  </div>
+      </li>
 
-</div>
+      
+
+      <?php the_content(); ?>
+    </ul>
+  </div> 
+</div><!-- .entry-content -->
 
 
+
+<!-- 
+
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/JcNBSjHZ0B0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+ -->
